b26be3d9cb06049c7b8ab10b719833f8917eb3b1
Alejandro Gutierrez <lovexknife@outlook.com>
2021-01-03 15:39:54 -0600

    started on project

---
+++ src/substitution.js
@@ -0,0 +1,15 @@
+function substitution(input, alphabet, encode = true){
+if (alphabet.length !== 26) return false; 
+let arr = input.split(''); 
+let alfa = "abcdefghijklmnopqrstuvwxyz"
+alfa = alfa.split('')
+//the input and the alphabet now an array of characters and spaces
+for(ltr in arr){
+    if (ltr !== " "){
+
+    }
+}
+
+}
+
+module.exports = substitution;
