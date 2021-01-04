02ee936f9bda94e0d94aba74d19aa5d1386a5b12
Alejandro Gutierrez <lovexknife@outlook.com>
2021-01-03 19:51:54 -0600

    substitution works now

--- src/substitution.js
+++ src/substitution.js
@@ -1,15 +1,47 @@
 function substitution(input, alphabet, encode = true){
 if (alphabet.length !== 26) return false; 
-let arr = input.split(''); 
-let alfa = "abcdefghijklmnopqrstuvwxyz"
-alfa = alfa.split('')
-//the input and the alphabet now an array of characters and spaces
-for(ltr in arr){
-    if (ltr !== " "){
-
+let inputArr = input.toLowerCase().split('');
+let cipherAlpha = alphabet.split(''); 
+let regularAlfa = "abcdefghijklmnopqrstuvwxyz"
+regularAlfa = regularAlfa.split('')
+//the input and the alphabet are now an array of characters and spaces
+if(encode === true){ //if encode is true, encode the input
+    for(let ltr in inputArr){ //check every letter in the input
+    if (inputArr[ltr] !== " "){ //if it's not a space --
+        let found 
+        for(let x in regularAlfa){ 
+            //check which letter in the regular alphabet matches the checked letter,
+            //once found, replace checked letter with the letter on the cipher that shares the index with the regular leter found
+            //set the found flag to true to move on to the next letter
+            if (inputArr[ltr] === regularAlfa[x] && !found){
+                inputArr[ltr] = cipherAlpha[x]
+                found = true
+            }
+        }
+    }
     }
 }
-
+if(encode === false){ //if encode is false, decode the input
+    for(let ltr in inputArr){ //check every letter in the input
+    if (inputArr[ltr] !== " "){ //if it's not a space --
+        let found 
+        for(let x in cipherAlpha){ 
+            //check which letter in the cipher alphabet matches the checked letter,
+            //once found, replace checked letter with the letter on the regular alphabet that shares the index with the cipher leter found
+            //set the found flag to true to move on to the next letter
+            if (inputArr[ltr] === cipherAlpha[x] && !found){
+                inputArr[ltr] = regularAlfa[x]
+                found = true
+            }
+        }
+    }
+    }
 }
+return inputArr.join('');
+}
+
+
+console.log(substitution("ale x","xoyqmcgrukswaflnthdjpzibev"))
+console.log(substitution("xwm b","xoyqmcgrukswaflnthdjpzibev",false))
 
 module.exports = substitution;
